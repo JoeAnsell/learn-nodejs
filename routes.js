6a950f53972adc2b20568da09c7633768eff7c6f
Joseph Ansell <anselljoseph19@gmail.com>
2022-08-24 09:50:44 +0100

    corrected file writing

--- routes.js
+++ routes.js
@@ -8,18 +8,19 @@ const requestHandler = (req, res) => {
     console.log(method);
     res.setHeader("Conent-Type", "text/html");
     res.write("<html>");
-    res.write("<header><title>Cunt</title></header>");
+    res.write("<header><title>Hello</title></header>");
     res.write(
-      "<body><h1>Welcome you cunt</h1><form action='/create-user' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+      "<body><h1>Welcome youdddd</h1><form action='/create-user' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
     );
     res.write("</html>");
     return res.end();
   }
+  ``;
   if (url === "/users") {
     console.log(method);
     res.setHeader("Conent-Type", "text/html");
     res.write("<html>");
-    res.write("<header><title>User Cunts</title></header>");
+    res.write("<header><title>Users</title></header>");
     res.write(
       "<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>"
     );
@@ -32,15 +33,22 @@ const requestHandler = (req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
+      const message = parseBody.split("=")[0];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
       console.log(message);
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
+  res.setHeader("Conent-Type", "text/html");
+  res.write("<html>");
+  res.write("<header><title>Createds</title></header>");
+  res.write("<body><h1>Creat user page</h1></body>");
+  res.write("</html>");
 };
 
 module.exports = requestHandler;
