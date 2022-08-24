7e1dbe5c07be4b27119512184861c9b939a800c8
Joseph Ansell <anselljoseph19@gmail.com>
2022-08-24 10:27:35 +0100

    set up nodemon to work with debugger

--- routes.js
+++ routes.js
@@ -35,7 +35,7 @@ const requestHandler = (req, res) => {
     });
     return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[0];
+      const message = parseBody.split("=")[1];
       fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
         res.setHeader("Location", "/");
